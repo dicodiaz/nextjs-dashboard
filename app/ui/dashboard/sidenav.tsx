633f6189e39d4219132bfaf1aef430c4dfa1ea70
Dico Diaz <dicodiaz@gmail.com>
2025-05-08 17:04:34 -0500

    revert Chapter 10 - Partial Prerendering

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -4,8 +4,6 @@ import { PowerIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { FC } from 'react';
 
-export const experimental_ppr = true;
-
 const SideNav: FC = () => {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
